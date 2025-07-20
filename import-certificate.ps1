b238221bca52235ab072cbdf9f677a8a49882238
Józef Podlecki <jozef.witold.podlecki@gmail.com>
2025-07-20 01:37:03 +0100

    suppress output from ci, try fix deploy

--- import-certificate.ps1
+++ import-certificate.ps1
@@ -1,5 +1,5 @@
 New-Item -ItemType directory -Path certificate | Out-Null
 Set-Content -Path certificate/tempCert.txt -Value $env:WINDOWS_CERTIFICATE
-certutil -decode certificate/tempCert.txt certificate/certificate.pfx
+certutil -decode certificate/tempCert.txt certificate/certificate.pfx | Out-Null
 Remove-Item -path certificate -include tempCert.txt
-Import-PfxCertificate -FilePath certificate/certificate.pfx -CertStoreLocation Cert:\CurrentUser\My -Password (ConvertTo-SecureString -String $env:WINDOWS_CERTIFICATE_PASSWORD -Force -AsPlainText)
+Import-PfxCertificate -FilePath certificate/certificate.pfx -CertStoreLocation Cert:\CurrentUser\My -Password (ConvertTo-SecureString -String $env:WINDOWS_CERTIFICATE_PASSWORD -Force -AsPlainText) | Out-Null
