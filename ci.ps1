55afdb4f36bbf93845a5ac5bd4db44e33a9b1a3b
Józef Podlecki <jozef.witold.podlecki@gmail.com>
2025-07-20 01:07:07 +0100

    fix rustc v [skip ci]

--- ci.ps1
+++ ci.ps1
@@ -1,2 +1,5 @@
 $msg = git log -1 --pretty=%B
-if ($msg -match '\[skip ci\]') { throw "Skipping CI as requested." }
+if ($msg -match '\[skip ci\]') { 
+    Write-Host "Skipping CI as requested."
+    exit(1)
+}
